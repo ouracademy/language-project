a1cf3616dafbb7d758a815df2d8bdf92120120ae
Dede1421 <mauricioandre91@gmail.com>
2026-03-09 13:06:35 -0500

    style: change design in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,27 @@ export default function Home() {
     <div className={styles.page}>
       <main>
         <Box>
-          <Typography variant="h2" gutterBottom>Welcome to iLangue</Typography>
-          <Typography sx={{ fontSize: "20px", paddingBottom: "20px" }}>Looking for learning natives languages ?</Typography>
+          <Typography variant="h2" gutterBottom>
+            Bienvenido a iLangue
+          </Typography>
+          <Typography sx={{ fontSize: "20px", paddingBottom: "20px" }}>
+            ¿Buscas aprender lenguas nativas ?
+          </Typography>
         </Box>
-        <Box style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
-          <Button variant="contained" style={{ color: "#000"}} sx={{ background: "#ffff" }}>Enter and watch</Button>
-        </Box>        
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{ background: "#3C8D80", color: "#ffff" }}
+          >
+            Enter and watch
+          </Button>
+        </Box>
       </main>
     </div>
   );
